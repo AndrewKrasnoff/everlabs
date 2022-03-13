9ca331127a2ea3f9470494ef7ad53d4fa3d4b25c
Andrew Krasnoff <andrew.krasnov1979@gmail.com>
2022-03-13 21:35:16 +0200

    search errors fixed

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,9 +5,10 @@ class SearchesController < ApplicationController
 
     sort_string = ''
     sort_string = params[:sort_field_1] + ' ' + params[:sort_order_1] if params[:sort_field_1] != 'none'
-    sort_string = sort_string + ', ' + params[:sort_field_2] + ' ' + params[:sort_order_2] if params[:sort_field_2] != 'none'
-    sort_string = sort_string + ', ' + params[:sort_field_3] + ' ' + params[:sort_order_3] if params[:sort_field_3] != 'none'
-
+    sort_string += ', ' if sort_string != ''
+    sort_string += params[:sort_field_2] + ' ' + params[:sort_order_2] if params[:sort_field_2] != 'none'
+    sort_string += ', ' if sort_string != '' && params[:sort_field_3] != 'none'
+    sort_string += params[:sort_field_3] + ' ' + params[:sort_order_3] if params[:sort_field_3] != 'none'
     @posts = @posts.order(sort_string)
 
     @posts = @posts.first(5) if params[:show_result] == 'show_first'
